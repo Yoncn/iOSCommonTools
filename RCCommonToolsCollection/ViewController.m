87e9d6488c2876ebeb514351de7e59468cf52a3b
rongchen <yoncn.rong@justalk.com>
2017-04-20 16:22:55 +0800

    Initial Commit

---
+++ RCCommonToolsCollection/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RCCommonToolsCollection
+//
+//  Created by rongchen on 2017/4/20.
+//  Copyright © 2017年 Yoncn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ RCCommonToolsCollectionTests/RCCommonToolsCollectionTests.m
@@ -0,0 +1,39 @@
+//
+//  RCCommonToolsCollectionTests.m
+//  RCCommonToolsCollectionTests
+//
+//  Created by rongchen on 2017/4/20.
+//  Copyright © 2017年 Yoncn. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RCCommonToolsCollectionTests : XCTestCase
+
+@end
+
+@implementation RCCommonToolsCollectionTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
